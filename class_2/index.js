b5988dbd591e2ec629f763ba16a1d262087af5d8
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-10 12:00:51 +0500

    Data Types

---
+++ class_2/index.js
@@ -0,0 +1,33 @@
+// Data types in javascript
+var num1= "asdfghj"
+var num2= 24
+var num3= 25.0897
+console.log(num1)
+console.log(num1, num2, num3)
+
+// add three variable '+' in math operation
+var num01= 5
+var num02= 5
+var num03= 5
+console.log(num01+num02+num03)
+
+// '+' concat operation
+var no1= "5"
+var no2= 5
+var no3= 5
+console.log(no1+no2+no3)
+
+// change datatype
+// no1= parseInt(no1)
+no1= Number(no1)
+console.log(no1+no2+no3)
+
+no2= no2.toString()
+console.log(no1+no2+no3)
+
+// check datatype of variable
+var firstName= "hamza"
+var age= 25
+var height= 5.6
+console.log(typeof(firstName),typeof(age),typeof(height))
+
