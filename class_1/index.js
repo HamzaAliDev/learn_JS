56abae84d44b0565f90e10be71993a32072bfc8d
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-10 11:54:31 +0500

    create variable

---
+++ class_1/index.js
@@ -0,0 +1,11 @@
+// practise for variables
+var name = "Ali Hamza"
+var age = 20
+var height = 5.6
+console.log(age, name);
+
+//write program to add two numbers
+var num1 = 3
+, num2 = 3
+, result = num1 + num2
+console.log(result);
