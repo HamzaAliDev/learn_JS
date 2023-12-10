48b47c985cbac8e91d306cc01d1ac5883ed5e051
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-10 11:49:18 +0500

    hellow world

---
+++ class_0/alert.js
@@ -0,0 +1,18 @@
+"use strick"
+let sir;
+user='ahmad'
+alert(user)
+
+// other method for variable 
+let user='ahmad' , age='45' , chat='hellow' 
+alert(user)
+alert(age)
+alert(chat)
+
+// other method for variable
+let data="ali"
+, sms="good"
+, bag="brown"
+alert(data + sms + bag)
+// alert(sms)
+// alert(bag)
