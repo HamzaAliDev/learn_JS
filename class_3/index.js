771d36ae1647b51bbf0cc20b2ffa3692e08c13f9
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-13 18:41:24 +0500

    Temp. converter

---
+++ class_3/index.js
@@ -0,0 +1,11 @@
+// write a program to solve simple problem
+// convert 5 C temperature into F.
+// formula (0 c *9/5) + 32
+var temInC= 5;
+var temInF= (temInC * 9/5 ) + 32
+console.log("Temperature in F", temInF)
+
+// preferences
+//   * /  both have sme level
+//  + - both have same level
+//  jo cheez first aay gi pehly who operate ho gi
