e5b3e5f4ae17bf186f26fdaae6060c9b9ff75a9d
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-13 18:46:52 +0500

    Conditional Structure

---
+++ class_5/index.js
@@ -0,0 +1,36 @@
+// Topic conditional structure
+
+// Default structure (line wise code execute)
+
+console.log("line 2");
+console.log("line 3");
+if (false) {
+    console.log("line 4");
+console.log("line 5");
+console.log("line 6");
+console.log("line 7");
+}
+else{
+console.log("line 8");
+console.log("line 9");
+console.log("line 10");
+console.log("line 11");
+console.log("line 12");
+}
+
+var num1= 5
+var num2= 6
+if (num1 > 10) {
+    console.log("num1 = 5")
+}
+else{
+    console.log("not correct");
+}
+var num3= 8
+var num4= 9
+if (num3/2 ===0) {
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
