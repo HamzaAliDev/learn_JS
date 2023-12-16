84f09f6904b70ece748527d60283279e0678526b
Ali Hamza <hamzamashooq492@gmail.com>
2023-12-16 22:24:34 +0500

    Loop

---
+++ class_7/index.js
@@ -0,0 +1,35 @@
+// "for" loop in javascript.
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+
+// its not a proper way, in this situation we use loop to execute the code many times.
+for (let index = 0; index < 5; index++) {
+    console.log("console loop");
+}
+
+// execute the loop 10 times
+for (let i = 0; i <5; i++) {
+    console.log("jnb");
+}
+
+// other types of loop "while loop"
+var num=1
+while (num<5) {
+    console.log("while loop",num);
+    num++
+}
+
+// "do while loop"
+var num2=0
+do
+ {console.log("do while loop");
+    num2++
+} while (num2 < 5);
